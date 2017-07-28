4d955184f1bd50b4a7cbd76d15dc967c222e4c4e
HybridAir <s.tratton.blake@gmail.com>
2017-07-28 00:58:43 -0400

    Cleanup and commenting

--- src/disp/bootAnim.cpp
+++ src/disp/bootAnim.cpp
@@ -1,109 +1,49 @@
+// handles all boot animation related code
 #include "disp/bootAnim.hpp"
 
 
 // plays the boot animation
 void playBootAnim() {
-    // flash top, left, right, then all on for a delay
-    // turn all off then flicker on each big number position (888.8 or whatever)
-    // when all are on, turn all off, then aftwer a short delay display a 0 in the first position along with all other data
-
-
     playLogo();
     delay(1000);
     lcd.clear();
     delay(200);
     playNumbers();
-
 }
 
+
+// flicker class descriptor
 Flicker::Flicker() {
-    randomSeed(analogRead(A0));
-    rand = random(1, 4);
-    finished = false;
+    randomSeed(analogRead(A0));                                                 // get a random seed for the rng
+    rand = random(1, 4);                                                        // selected a random number between 1 and 4, this determines the flicker amount/duration
+    //finished = false;
 }
 
+
+// updates the flicker animation, returns the current state
+// the returned state is used to determine whether you need to display or clear whatever you're trying to flicker
 bool Flicker::run() {
-    if(rand > frame) {
+    if(rand > cycle) {                                                          // only animate up to the random flicker amount we got before
         if(currentState) {
             currentState = false;
-            frame++;
+            cycle++;                                                            // advance the cycle value after each complete on/off cycle
         }
         else {
             currentState = true;
         }
     }
     else {
-        currentState = true;
+        currentState = true;                                                    // the flicker animation finishes with the animated thing being displayed
         finished = true;
     }
     return currentState;
 }
 
 
-
-
-void playNumbers() {
-    writeBigNumChars();
-    Flicker num0;
-    Flicker num1;
-    Flicker num2;
-
-    uint16_t previousMillis = 0;
-    bool flickering = true;
-    uint8_t frame = 0;
-
-    while(flickering) {
-        uint16_t currentMillis = millis();
-
-        if (currentMillis - previousMillis >= 50) {
-            previousMillis = currentMillis;
-
-            if(num1.run()) {
-                drawBigNum(3, '8');
-            }
-            else {
-                clearBigNum(3);
-            }
-
-            if(frame > 1) {
-                if(num0.run()) {
-                    drawBigNum(0, '1');
-                }
-                else {
-                    clearBigNum(0);
-                }
-            }
-
-            if(frame > 4) {
-                if(num2.run()) {
-                    drawBigNum(7, '8');
-                }
-                else {
-                    clearBigNum(7);
-                }
-            }
-
-            frame++;
-            if(num0.finished && num1.finished && num2.finished) {
-                flickering = false;
-            }
-        }
-    }
-
-    delay(500);
-    clearBigNum(0);
-    clearBigNum(3);
-    clearBigNum(7);
-    delay(40);
-    drawBigNum(7, '0');
-    delay(50);
-    lcd.setCursor(12, 0);
-    lcd.print("KM/H");
-
-}
-
-
+// the first part of the boot animation, displays the logo and the device info
 void playLogo() {
+
+    // load the custom logo characters into the lcd's cgram
     lcd.createChar(0, (uint8_t *) top1);
     lcd.createChar(1, (uint8_t *) top2);
     lcd.createChar(2, (uint8_t *) left2);
@@ -111,19 +51,23 @@ void playLogo() {
     lcd.createChar(4, (uint8_t *) right2);
     lcd.createChar(5, (uint8_t *) right1);
 
+    // create a flicker class for each title/subtitle
     Flicker title;
     Flicker subtitle;
 
+    // set up the animation timing variables
     bool playing = true;
     uint16_t previousMillis = millis();
     uint8_t frame = 0;
 
+    // the entire animation is controlled in here
     while(playing) {
         uint16_t currentMillis = millis();
 
-        if (currentMillis - previousMillis >= 25) {
+        if (currentMillis - previousMillis >= 25) {     // display is updated every 25 ms
             previousMillis = currentMillis;
 
+            // after frame 50, animate the title and subtitle
             if(frame > 50) {
                 if((frame % 2) == 0) {
                     if(title.run()) {
@@ -151,11 +95,12 @@ void playLogo() {
             }
             frame++;
 
-            if(title.finished && subtitle.finished) {
-                playing = false;
+            if(title.finished && subtitle.finished) {       // the title and subtitle are the last elements to be animated in this section
+                playing = false;        // this section of the boot animation is considered to be complete
             }
         }
 
+        // animate the logo
         switch(frame) {
             case 0:
                 logoTop(1, 0);
@@ -210,4 +155,75 @@ void logoRight(uint8_t x, uint8_t y) {
 void clearLogo(uint8_t x, uint8_t y) {
     lcd.setCursor(x, y);
     lcd.print("  ");
+}
+
+
+// plays the speedometer number part of the animation
+void playNumbers() {
+    writeBigNumChars();         // put the custom big number characters into the lcd's cgram
+
+    // create a flicker class for each number position
+    Flicker num0;
+    Flicker num1;
+    Flicker num2;
+
+    // set up the animation timing variables
+    uint16_t previousMillis = 0;
+    bool flickering = true;
+    uint8_t frame = 0;
+
+    // the flickering part of the animation
+    // each number position is flickered on in a 2-1-3 order
+    // 188 is used to give the illusion of a segmented display defaulting to all segments lit or something idk
+    while(flickering) {
+        uint16_t currentMillis = millis();
+
+        if (currentMillis - previousMillis >= 50) {         // update the display every 50 ms
+            previousMillis = currentMillis;
+
+            // run the second position flicker
+            if(num1.run()) {
+                drawBigNum(3, '8');
+            }
+            else {
+                clearBigNum(3);
+            }
+
+            // run the first position flicker starting at frame 1
+            if(frame > 1) {
+                if(num0.run()) {
+                    drawBigNum(0, '1');
+                }
+                else {
+                    clearBigNum(0);
+                }
+            }
+
+            // run the third position flicker starting at frame 4
+            if(frame > 4) {
+                if(num2.run()) {
+                    drawBigNum(7, '8');
+                }
+                else {
+                    clearBigNum(7);
+                }
+            }
+
+            frame++;
+            if(num0.finished && num1.finished && num2.finished) {
+                flickering = false;
+            }
+        }
+    }
+
+    // the second part, transitioning to the final speedometer display
+    delay(500);
+    clearBigNum(0);
+    clearBigNum(3);
+    clearBigNum(7);
+    delay(40);
+    drawBigNum(7, '0');
+    delay(50);
+    lcd.setCursor(12, 0);
+    lcd.print("KM/H");
 }
--- src/disp/bootAnim.hpp
+++ src/disp/bootAnim.hpp
@@ -6,7 +6,8 @@
 
 extern ST7032 lcd;
 
-//custom lcd characters for the logo
+
+// custom lcd characters for the logo
 static const uint8_t top1[] = {0x1, 0x1, 0x3, 0x3, 0x7, 0x7, 0x3, 0x1};
 static const uint8_t top2[] = {0x10, 0x10, 0x18, 0x18, 0x1c, 0x1c, 0x18, 0x10};
 static const uint8_t left2[] = {0x18, 0x1c, 0x1e, 0x1e, 0x1e, 0x18, 0x0, 0x0};
@@ -14,16 +15,20 @@ static const uint8_t left1[] = {0x0, 0x0, 0x1, 0x3, 0x7, 0xf, 0x1e, 0x0};
 static const uint8_t right2[] = {0x3, 0x7, 0xf, 0xf, 0xf, 0x3, 0x0, 0x0};
 static const uint8_t right1[] = {0x0, 0x0, 0x10, 0x18, 0x1c, 0x1e, 0xf, 0x0};
 
+
+// class used to create animations that randomly flicker on and off a set amount of times
+// it creates a sort of flicker on effect, inspiration: http://sxo-sxo.tumblr.com/post/140167380273/alex-townsend
 class Flicker {
     public:
-        uint8_t frame = 0;
-        uint8_t rand;
-        bool currentState = false;
-        bool finished = false;
+        uint8_t cycle = 0;          // the current flicker cycle
+        uint8_t rand;               // the random amount of times to flicker, determines how many frames the animation lasts
+        bool currentState = false;  // whether what is being animated should be displayed or not
+        bool finished = false;      // the flicker doesn't run forever you know
 
-        Flicker();
-        bool run();
+        Flicker();                  // sets up the random number generator seed
+        bool run();                 // run this each frame of the animation, or whenever you want it to update
 };
+
 
 void playBootAnim();
 static void playLogo();
